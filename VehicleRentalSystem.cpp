f975c61cdfd137ed4c6a89769cdb58214c145abf
mengkim168 <mengkimtang@gmail.com>
2024-04-06 18:20:37 +0700

    Finished

--- VehicleRentalSystem.cpp
+++ VehicleRentalSystem.cpp
@@ -1,3 +1,12 @@
+/*
+    Group 6 ES1
+    1. Hak Layheang     ID: B20235881
+    2. Heach Tevin      ID: B20236016
+    3. Khat Panhadath   ID: B20232604
+    4. Tang MengKim     ID: B20231674
+    5. Sung Sovansak    ID: B20231056
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -14,19 +23,18 @@ public:
     Vehicle(const string &vmake, const string &vmodel, const int &vspeed, const float &vdaysFee)
         : make(vmake), model(vmodel), speed(vspeed), daysFee(vdaysFee) {}
     
+    // Getter
     virtual string getType() const = 0 ;
-
     string getModel() const{
         return model;
     }
     float getDaysFee() const{
         return daysFee;
     }
-
-    void display() const {
+    // Display function
+    void display() const{
         cout << " " << make << "\n";
-        cout << "    " << model << "(Speed: " << speed << " km/hr)"
-             << "\n";
+        cout << "    " << model << "(Speed: " << speed << " km/hr)" << "\n";
         cout << "    Rent/day: $" << daysFee << endl;
     }
 };
@@ -73,7 +81,6 @@ public:
     Rental(Vehicle* vehicles): vehicle(vehicles){
         this -> setStatus("Available");
     }
-
     // setter
     void setDaysRented(const int &days){
         daysRented = days;
@@ -81,56 +88,130 @@ public:
     void setStatus(const string &stat){
         this -> status = stat;
     }
-
     // getter
+    int getDayRented() const { 
+        return this -> daysRented; 
+        }
     string getStatus() const {
         return this -> status;
     }
     string getType() const {
         return vehicle -> getType();
     }
+    string getModel(){
+        return vehicle -> getModel();
+    }
     
     // display
-    void displaytype(){
+    void display(){
         vehicle -> display();
         cout << "    Status: " << status << endl;
     }
-    // calculator function
-    float rentFee() const {
-        return vehicle -> getDaysFee() * daysRented;
+    void displayRentalFee(){
+        cout << " ---------"<< endl;
+        cout << this->getModel() << " is rented for "<< this->getDayRented() << "days = "<< this ->rentalFee()<<"$"<<endl;
     }
-    
+    // Rental Function
+    void rented(const int& days){
+        this->setDaysRented(days);
+        this ->setStatus("Rented");
+    }
+    void returned(){
+        if(this -> getStatus() == "Returned"){
+            this -> setDaysRented(0);
+            this -> setStatus("Available");
+            cout << "====Return Success!" << endl;
+        }
+        else{
+            cout << "===Return Unsuccess! Try Again!" << endl;
+        }
+    }
+    // calculator function
+    float rentalFee() const {
+        return (vehicle->getDaysFee()) * (this->daysRented);
+    }
 };
 class Customer {
-    private:
-        string name;
-        vector<Rental> rentedVehicle;
-    public:
-    Customer(const string& cname) :name(cname){}      
-
-
+private:
+    string name;
+    vector<Rental> rentedVehicle;
+public:
+    Customer(){};
+    Customer(const string &cname) : name(cname) {}
+    // Getter
     string getName() const { return name; }
-    
-    void customerInformation(){
-        cout << "Enter Your Name Here: ";
-        cin >> name;
+
+    // Member function
+    void rented(Rental &vehicle, const int &days){
+        if (vehicle.getStatus() == "Available"){
+            vehicle.setDaysRented(days);
+            vehicle.setStatus("Rented");
+            vehicle.rented(days);
+            rentedVehicle.push_back(vehicle);
+            vehicle.displayRentalFee();
+            cout << "====Rental Success!" << endl;
+        }
+        else{
+            cout << "===Rental Unsuccess! Try Again!" << endl;
+        }
+    }
+    void returnVehicle(Rental &vehicleRe){
+        int index = 0;
+        for (Rental &vehicle : rentedVehicle){
+            if (vehicle.getModel() == vehicleRe.getModel()){
+                vehicle.setStatus("Returned");
+                break;
+            }
+            index++;
+        }
+        if (rentedVehicle.size() > 0){
+            rentedVehicle.erase(rentedVehicle.begin() + (index - 1));
+        }
+    }
+    // Display
+    void displayRentals() const{
+        int i = 1;
+        cout << "Name: " << name << endl;
+        cout << "================" << endl;
+        double totalFees = 0;
+        for (Rental vehicle : rentedVehicle){
+            cout << i <<". ";
+            vehicle.display();
+            cout << " Rental fee: $" << vehicle.rentalFee() << endl;
+            cout << "============================" << endl;
+            totalFees += vehicle.rentalFee();
+            i++;
+        }
+        cout << " Fee sum: $" << totalFees << endl;
+    }
+    // Use to display all Vehicle is rented
+    void displayRented(){
+        for(Rental& vehicle : rentedVehicle){
+        vehicle.display();
+        cout << "   Rented : "<<vehicle.getDayRented() <<"Days";
+        cout << " = " << vehicle.rentalFee() <<"$" <<endl;
+        }
+    }
+    Rental getRentedVehicle(const int &index){
+        return rentedVehicle[index];
+    }
+    int getRentedVehicleCount(){
+        return rentedVehicle.size();
     }
 };
 
 // main funtion initializer
+bool closeShop();
 void displayMenu();
 int getIntInput();
-vector<string> allVehicleType(const vector<Rental>& vehicles);
-void selectVehicle(const vector<Rental>& vehicles, const string& vtype);
-void rentalVehicle(const vector<Rental>& vehicles);
+void showCustomers(const vector<Customer>& customers);
+vector<string> getUniqueVehicleType(const vector<Rental>& vehicles);
+void rentalVehicle(vector<Rental>& vehicles,Customer& currentCustomer);
+void returnVehicle(vector<Rental>& vehicles, Customer& currentCustomer);
 void displayType (const vector<string> types);
-bool openShop();
 
 int main(){
-    Customer customer("Customer");
-    // customer.customerInformation();
-    cout << customer.getName();
-
+    vector<Customer> customers;
     vector<Rental> vehicles;
 
     Car car1("Sedan", "Toyota Corolla", 120, 50);
@@ -157,45 +238,80 @@ int main(){
 
     int choice;
     bool explore = true;
-    while(openShop()){
-
-    };
-
-    bool exit = false;
-    while (!exit){
-
-        displayMenu();
-        cin >> choice;
-        if(choice >= 0 && choice < 4){
-            switch (choice)
-            {
-            case 0:
-                cout << "Goodbye, " << endl;
-                exit = true;
-                break;
-            case 1:
-                cout << "Selected \"Rent Vehicle\": "<< endl;
-                rentalVehicle(vehicles);
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            default:
-                exit = true;
+    // main block running
+    while (closeShop()){
+        showCustomers(customers);
+        // innit customer
+        string name;
+        int cusIndex = 0;
+        bool oldCustomer = false;
+        cout << "Enter name: ";
+        cin >> name;
+        for (Customer customer : customers){
+            if (customer.getName() == name){
+                oldCustomer = true;
                 break;
             }
-        }else{
-            cout << "Invalid choice" << endl;
+            else{ cusIndex++; }
+        }
+        if (oldCustomer == false){
+            Customer newCustomer(name);
+            customers.push_back(newCustomer);
+            cusIndex = customers.size() - 1;}
+
+        Customer &currentCustomer = customers[cusIndex];
+
+        bool exit = false;
+        while (!exit) {
+            displayMenu();
+            choice = getIntInput();
+            if (choice >= 0 && choice < 4){
+                switch (choice){
+                case 0:
+                    cout << endl;
+                    cout << "Goodbye, " << endl;
+                    exit = true;
+                    break;
+                case 1:
+                    cout << "Selected \"Rent Vehicle\": " << endl;
+                    rentalVehicle(vehicles, currentCustomer);
+                    break;
+                case 2:
+                    cout << "Selected \"Return Vehicle\": " << endl;
+                    returnVehicle(vehicles, currentCustomer);
+                    break;
+                case 3:
+                    cout << "Selected \"View Rented Vehicle\": " << endl;
+                    currentCustomer.displayRentals();
+                    break;
+                default:
+                    exit = true;
+                    break;
+                }
+            }
+            else{
+                cout << "Invalid choice" << endl;
+            }
         }
     }
     return 0;
 }
+// =====End Main=====
 
-void displayMenu()
-{  
+bool closeShop(){
+  //function to return true false, to switch between users
+  int answer;
+  cout << endl;
+  cout << "Do you want to rent any vehicle?" << endl; 
+  cout << "1. Rent Vehicle" << endl; 
+  cout << "0. Exit" << endl; 
+  cout << "Enter your choice: ";
+  answer = getIntInput();
+
+  if(answer == 1){ return true; }
+  else {cout << "====Shop closed!====" << endl; return false; }
+}
+void displayMenu(){  
     cout << endl;
     cout << "====Welcome the Rental Garage!====" << endl;
     cout << "What would you like to do?" << endl;
@@ -204,56 +320,26 @@ void displayMenu()
     cout << "3. View Rented Vehicle" << endl;
     cout << "0. Exit" << endl;
     cout << "\n Enter a choice from menu: ";
-
 }
-void rentalVehicle(const vector<Rental>& vehicles){
-    vector<string> types = allVehicleType(vehicles);
-    displayType(types);
-    cout << "type size = "<< types.size() <<endl;
-    cout << "Enter Number: "; 
-    int choosetype = getIntInput();
-
-    if (choosetype > 0 && choosetype <= types.size()){
-        string type = types[choosetype - 1];
-        selectVehicle(vehicles,type);
-        return;
-    }         
-}
-
-void displayType (const vector<string> types){
+void displayType(const vector<string> types){
     for (int i = 1; i <= types.size(); i++){
-        cout << i << ". "<< types[i-1] <<endl;
+        cout << i << ". " << types[i - 1] << endl;
     }
+    cout << "Enter Number: ";
 }
-
-void selectVehicle(const vector<Rental>& vehicles, const string& vtype){
-    vector<Rental> vehicleList; 
-    int i = 1;
-    // looping and display all details, using rental member function display
-    for (Rental vehicle : vehicles)
-    {
-        if( vehicle.getType() == vtype ){
-        cout << "    --------------" << endl;
-        cout << i << ". ";
-        vehicle.displaytype();
-        vehicleList.push_back(vehicle);
-        i++;
+//a quick loop to read through the names of customers<vector>
+void showCustomers(const vector<Customer> &customers){
+    if (customers.size() > 0){
+        cout << "Customer served: ";
+        for (Customer customer : customers){
+            cout << "| " << customer.getName() << " | ";
         }
-    }
-    cout << "Choose " << vtype << " you want to rent :";
-        int chooseList = getIntInput();
-    if (chooseList > 0 && chooseList <= vehicleList.size()){
-        cout << "okay" << endl;
-
-        
-    }else{
-        chooseList = 1;
-    cout<<"no";
+        cout << endl;
     }
 }
 
-
-vector<string> allVehicleType(const vector<Rental>& vehicles){
+// To get unique of vehicle type 
+vector<string> getUniqueVehicleType(const vector<Rental>& vehicles){
     // Create a vector to store unique types
     vector<string> Vtype;
     for (const Rental& vehicle: vehicles){
@@ -261,16 +347,83 @@ vector<string> allVehicleType(const vector<Rental>& vehicles){
 
         // Check if the type is not already in the vector
         if (find(Vtype.begin(),Vtype.end(),types) == Vtype.end()){
-            // If not found, display the type and add it to the vector
             Vtype.push_back(types);
         }
     };
     return Vtype;
 }
 
-bool openShop(){
-    string customerName;
-    
+void rentalVehicle(vector<Rental> &vehicles, Customer &currentCustomer){
+    // get all type of vehicles and display
+    vector<string> types = getUniqueVehicleType(vehicles);
+    displayType(types);
+
+    cout << "Enter a choice from type: ";
+    int choosetype = getIntInput();
+
+    if (choosetype > 0 && choosetype <= types.size()){
+        int i = 1;
+        vector<Rental> vehicleList;
+        string type = types[choosetype - 1];
+        // looping and display all details, using Rental member function display
+        for (Rental &vehicle : vehicles){
+            if (vehicle.getType() == type){
+                cout << "    --------------" << endl;
+                cout << i << ". ";
+                vehicle.display();
+                vehicleList.push_back(vehicle);
+                i++;
+            }
+        }
+        // User choose Vehicle Number
+        cout << "Choose " << type << " you want to rent : ";
+        int chooseList = getIntInput();
+        // Check User input
+        if (chooseList > 0 && chooseList <= vehicleList.size()){
+            cout << "How many days do you want to rent? : ";
+            int days = getIntInput();
+            // Final Rented Process
+            for (Rental &vehicle : vehicles){
+                if (vehicle.getModel() == vehicleList[chooseList - 1].getModel()){
+                    currentCustomer.rented(vehicle, days);
+                    vehicle.rented(days);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void returnVehicle(vector<Rental>& vehicles, Customer& currentCustomer){
+    int totalVehicle = currentCustomer.getRentedVehicleCount();
+        // Display Rented Vehicle
+        for(int i = 0; i < totalVehicle; i++){
+            Rental vehicle = currentCustomer.getRentedVehicle(i);
+            cout << i + 1 <<". ";
+            currentCustomer.displayRented();
+        }
+    // Select Vehicle to Return
+    if (totalVehicle > 0){
+        cout << "Select your Vehicle : ";  
+        int index = getIntInput();
+        if (index <= totalVehicle){
+            Rental vehicleRe = currentCustomer.getRentedVehicle(index - 1);
+            cout << endl;
+            cout << "You have selected "<< vehicleRe.getModel();
+            currentCustomer.returnVehicle(vehicleRe);
+            for (Rental& vehicle : vehicles){
+                if(vehicle.getModel() == vehicleRe.getModel()){
+                    vehicle.setStatus("Returned");
+                    vehicle.returned();
+                    break;
+                }
+            }
+        }
+    }
+    else {
+        cout << endl;
+        cout << "===No Rented Vehicle in list!" <<endl;
+    }
 }
 // Function to validate integer input
 int getIntInput() {
@@ -279,14 +432,10 @@ int getIntInput() {
         cin >> i;
         // Check if the input operation failed
         if (cin.fail()) {
-            // Clear the error flag
             cin.clear();
             // Discard the invalid input
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            // Notify the user of the error
-        } else {
-            // Input operation succeeded, return the integer
-            return i;
-        }
+        } 
+        else { return i; }
     }
 }
